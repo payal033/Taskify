8beed8c13e461a135dd44885898ddf41d2c86074
Payal <payalkarwa99@gmail.com>
2024-12-08 18:21:23 +0530

    working with outputs & emitting data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, input, Input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,16 +15,16 @@ import { Component, computed, input, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input() avatar!: string;
-  // @Input() name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-  imagePath = computed(() => 'assets/users/' + this.avatar());
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
-
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
